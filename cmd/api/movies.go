420fc8fb48652571399deb73200c51592995827a
Bayashat <baiashat2002@gmail.com>
2023-10-07 12:46:40 +0600

    Return movie showing as JSON, Encoded Struct to JSON, Hiding struct fields in JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/Bayashat/Go_book-Greenlight/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	
-	fmt.Fprintf(w, "show the details of movie: %d\n", id)
+
+	// Create a new instance of the Movie struct, containing the ID we extracted from the URL and some dummy data.
+	// Also notice that we deliberately haven't set a value for the Year field.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
